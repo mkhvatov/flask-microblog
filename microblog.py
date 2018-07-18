ec5845053f21d38b5d7d349d6984d2131376c48d
Matvey Khvatov <matveykhvatov@gmail.com>
2018-07-18 22:44:37 +0300

    microblog after chapter5 flask-tutorial

--- microblog.py
+++ microblog.py
@@ -2,7 +2,6 @@ from app import app, db
 from app.models import User, Post
 
 
-# коментарий для изменения
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
