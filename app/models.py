ee3e1c868b9a5dddaa97970a902236d04ed907b5
Matvey Khvatov <matveykhvatov@gmail.com>
2018-07-19 18:40:06 +0300

    Added: 1-user profile page; 2-avatar for user profile page

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 from app import db, login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -19,6 +20,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 class Post(db.Model):
