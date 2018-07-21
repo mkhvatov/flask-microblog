243a0253a96a452d009e40c6c452c1fe04dad67a
Matvey Khvatov <matveykhvatov@gmail.com>
2018-07-21 10:18:05 +0300

    posts breakdown by pages was added

--- config.py
+++ config.py
@@ -16,3 +16,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['matveykhvatov@gmail.com']
+    POSTS_PER_PAGE = 3
