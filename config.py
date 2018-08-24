3280e76e4eea8e1dbd14a06210b9b6cf811f0b3a
mkhvatov <matveykhvatov@gmail.com>
2018-08-24 12:15:21 +0300

    config.py was updated

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['matveykhvatov@gmail.com']
+    ADMINS = ['username@gmail.com']
     POSTS_PER_PAGE = 20
