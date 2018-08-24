a94c48cd34026b5df57509835b9a750e465b9ce2
mkhvatov <matveykhvatov@gmail.com>
2018-08-24 16:26:28 +0300

    Microblog after chapter13: languages were added

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
-from flask import Flask
-from flask_login import LoginManager
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 from config import Config
 
@@ -20,10 +21,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-login.login_message = 'Пожалуйста, войдите, чтобы открыть эту страницу.'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 
 if not app.debug:
@@ -44,7 +46,8 @@ if not app.debug:
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
@@ -52,6 +55,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 from app import routes, models, errors
